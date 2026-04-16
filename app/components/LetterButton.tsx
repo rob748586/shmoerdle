ba0bfcd92a7fd43016d21a289ab0e573b5f4e0a3
rob748586 <rob748385@gmail.com>
2026-04-16 18:18:07 +0100

    tweaked css classes on buttons for dark and light modes

--- app/components/LetterButton.tsx
+++ app/components/LetterButton.tsx
@@ -5,13 +5,13 @@ export default function LetterButton(props: {
 }) {
   const { letter, disabled, onClick } = props;
   const buttonClass = disabled
-    ? "bg-gray-400 dark:bg-gray-700 text-white"
-    : "bg-white dark:bg-gray-800";
+    ? "bg-gray-400 dark:bg-gray-900 text-white dark:text-gray-600 cursor-not-allowed"
+    : "bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 ";
 
   return (
     <button
       disabled={disabled}
-      className={`w-8 h-8 md:w-12 md:h-12 flex items-center justify-center border border-gray-300 rounded-md md:text-lg font-bold text-gray-700 dark:text-gray-300 dark:border-gray-600 ${buttonClass}`}
+      className={`w-8 h-8 md:w-12 md:h-12 flex items-center justify-center border border-gray-300 rounded-md md:text-lg font-bold  dark:border-gray-600 ${buttonClass}`}
       onClick={() => onClick(letter)}
     >
       {letter}
