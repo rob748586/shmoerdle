7a41531d00e13d990742a33e4eccaf95b5afc8e4
rob748586 <rob748385@gmail.com>
2026-04-17 13:37:28 +0100

    Refactored functions for DeleteLetter, LetterEntered in main page, separating logic from display code

--- app/components/GuessHistoryBoard.tsx
+++ app/components/GuessHistoryBoard.tsx
@@ -1,15 +1,14 @@
 import { checkGuess } from "@/lib/CheckGuess";
 import LetterTile from "./LetterTile";
 
-export default function GuessStatusBar(props: {
+export default function GuessHistoryBoard(props: {
   guess: string;
   word: string;
-  key?: number;
 }) {
-  const { guess, word, key } = props;
+  const { guess, word } = props;
   const guessStatus = checkGuess(guess, word || "");
   return (
-    <div key={key} className="flex justify-center mt-4 gap-2">
+    <div className="flex justify-center mt-4 gap-2">
       <div className="flex gap-2">
         <LetterTile letter={guess[0] || ""} status={guessStatus[0]} />
         <LetterTile letter={guess[1] || ""} status={guessStatus[1]} />
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { useState, useEffect } from "react";
 import Keyboard from "./components/Keyboard";
 import LetterTile from "./components/LetterTile";
 import StatusBar from "./components/StatusBar";
-import GuessStatusBar from "./components/GuessStatusBar";
+import GuessHistoryBoard from "./components/GuessHistoryBoard";
 import BackSpace from "./components/BackSpace";
 
 export default function Home() {
@@ -40,6 +40,59 @@ export default function Home() {
       }
     }
   }, [words]);
+
+  function DeleteLetter() {
+    {
+      const newGuess = (guesses[guesses.length - 1] || "").slice(0, -1);
+      setGuesses([...guesses.slice(0, -1), newGuess]);
+    }
+  }
+
+  function LetterEntered(letter: string) {
+    // safety check to prevent letter entry if the game is already won or lost.
+    if (gameStatus !== GameStatus.Playing) {
+      return;
+    }
+
+    // If the last guess is empty, start a new guess with the clicked letter.
+    const newGuess = (guesses[guesses.length - 1] || "") + letter;
+    // If the new guess is 5 letters or less, update the current guess.
+    if (newGuess.length <= 5) {
+      setGuesses([...guesses.slice(0, -1), newGuess]);
+    }
+    // If the new guess is exactly 5 letters, finalize the guess and start a new empty guess.
+    if (newGuess.length === 5) {
+      // Update notFound letters based on the new guess, disabling
+      // letters on the keyboard that are not in the word.
+      let temp = [...notFound];
+      for (const letter of newGuess) {
+        if (!word?.toUpperCase()?.includes(letter.toUpperCase())) {
+          // If the letter is not in the word and not already in the notFound list, add it to the list.
+          if (!temp.includes(letter.toUpperCase())) {
+            temp = [...temp, letter.toUpperCase()];
+          }
+        }
+      }
+      // Update the notFound state with the new list of letters to disable on the keyboard.
+      setNotFound([...temp]);
+
+      // Finalize the current guess and start a new empty guess.
+      setGuesses([...guesses.slice(0, -1), newGuess, ""]);
+    }
+
+    // Test win/loss conditions after the new guess is added to the guesses list.
+    if (guesses.length > 0 && newGuess.toUpperCase() === word?.toUpperCase()) {
+      setGameStatus(GameStatus.Won);
+    }
+    if (
+      guesses.length >= 5 &&
+      newGuess.toUpperCase() !== word?.toUpperCase() &&
+      newGuess.length === 5
+    ) {
+      setGameStatus(GameStatus.Lost);
+    }
+  }
+
   const guess = guesses[guesses.length - 1] || "";
   return (
     <div className="flex flex-col flex-1 items-center justify-start bg-zinc-200 font-sans dark:bg-black ">
@@ -58,7 +111,11 @@ export default function Home() {
           {guesses.map((guess, index) => {
             if (index < guesses.length - 1) {
               return (
-                <GuessStatusBar guess={guess} word={word || ""} key={index} />
+                <GuessHistoryBoard
+                  guess={guess}
+                  word={word || ""}
+                  key={index}
+                />
               );
             }
           })}
@@ -86,65 +143,11 @@ export default function Home() {
                   letter={guess[4] || ""}
                   status={LetterStatus.Unused}
                 />
-                <BackSpace
-                  onClick={() => {
-                    const newGuess = (guesses[guesses.length - 1] || "").slice(
-                      0,
-                      -1,
-                    );
-                    setGuesses([...guesses.slice(0, -1), newGuess]);
-                  }}
-                />
+                <BackSpace onClick={() => DeleteLetter()} />
               </div>
               <Keyboard
                 notFound={notFound}
-                onLetterClick={(letter) => {
-                  if (gameStatus !== GameStatus.Playing) {
-                    return;
-                  }
-
-                  // If the last guess is empty, start a new guess with the clicked letter.
-                  const newGuess = (guesses[guesses.length - 1] || "") + letter;
-                  // If the new guess is 5 letters or less, update the current guess.
-                  if (newGuess.length <= 5) {
-                    setGuesses([...guesses.slice(0, -1), newGuess]);
-                  }
-                  // If the new guess is exactly 5 letters, finalize the guess and start a new empty guess.
-                  if (newGuess.length === 5) {
-                    // Update notFound letters based on the new guess, disabling
-                    // letters on the keyboard that are not in the word.
-                    let temp = [...notFound];
-                    for (const letter of newGuess) {
-                      if (
-                        !word?.toUpperCase()?.includes(letter.toUpperCase())
-                      ) {
-                        // If the letter is not in the word and not already in the notFound list, add it to the list.
-                        if (!temp.includes(letter.toUpperCase())) {
-                          temp = [...temp, letter.toUpperCase()];
-                        }
-                      }
-                    }
-                    // Update the notFound state with the new list of letters to disable on the keyboard.
-                    setNotFound([...temp]);
-
-                    // Finalize the current guess and start a new empty guess.
-                    setGuesses([...guesses.slice(0, -1), newGuess, ""]);
-                  }
-
-                  if (
-                    guesses.length > 0 &&
-                    newGuess.toUpperCase() === word?.toUpperCase()
-                  ) {
-                    setGameStatus(GameStatus.Won);
-                  }
-                  if (
-                    guesses.length >= 5 &&
-                    newGuess.toUpperCase() !== word?.toUpperCase() &&
-                    newGuess.length === 5
-                  ) {
-                    setGameStatus(GameStatus.Lost);
-                  }
-                }}
+                onLetterClick={(letter) => LetterEntered(letter)}
               />
             </>
           ) : null}
