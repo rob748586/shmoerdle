ca3e737be868adb5ecbf8f35d489d2bb027c41b4
rob748586 <rob748385@gmail.com>
2026-05-23 15:40:30 +0100

    Image and style sheet preloading added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <Head>
+        <Link rel="preload" href="/shmoerdle.png" as="image" type="image/png" />
+        <Link rel="preload" href="/globals.css" as="style" type="text/css" />
+      </Head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
