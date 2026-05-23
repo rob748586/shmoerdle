806c9c5e580a0dd220c35644de3d2dc0e9361aca
rob748586 <rob748385@gmail.com>
2026-05-23 14:12:01 +0100

    service worker and serwist added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts", // Path to your custom service worker source file
+  swDest: "public/sw.js", // Path where the generated service worker will be placed
+  cacheOnNavigation: true, // Whether to cache navigation requests
+  reloadOnOnline: true, // Whether to reload the page when the user goes back online
+  disable: process.env.NODE_ENV === "development", // Disable Serwist in development mode
+  // Your Serwist configuration options here
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
