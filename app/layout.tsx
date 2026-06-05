a392ef2a3b42251c3e54c444438d9a9c00e5f22d
rob748586 <rob748385@gmail.com>
2026-06-05 16:50:18 +0100

    updated link visibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,12 +37,15 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Analytics />
         {children}
-        <a
-          href="https://github.com/rob748586/shmoerdle"
-          className="text-center text-sm text-gray-700 absolute bottom-4 w-full"
-        >
-          Rob Hine, 2026. View source code on GitHub
-        </a>
+        <p className="text-center text-sm text-gray-700 absolute bottom-4 w-full">
+          Rob Hine, 2026.{" "}
+          <a
+            href="https://github.com/rob748586/shmoerdle"
+            className="text-blue-700 underline"
+          >
+            View source code on GitHub
+          </a>
+        </p>
       </body>
     </html>
   );
